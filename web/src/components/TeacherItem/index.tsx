0239df72f8215fe948127438c4b2ec8de99610b1
Diego Andrade <45301572+diegoAndrade777@users.noreply.github.com>
2020-08-06 15:02:59 -0300

    frontend finished

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,36 +1,59 @@
 import React from 'react';
 
+import api from '../../services/api';
+
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  user_id: number;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function creatNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/45301572?s=460&u=faeea688ef1f8468c7c7dc17103b749a88d05fea&v=4" alt="Diego Andrade"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Diego Andrade</strong>
-          <span>FrontEnd</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br/><br/>
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/Hora
-          <strong>R$80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
 
-        <button type="button">
+        <a 
+          target="blank"
+          onClick={creatNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}?text=Olá ${teacher.name}, ví que você dá aula no Proffy! Gostaria de mais detalhes dos seus serviços...`}
+        >
           <img src={whatsappIcon} alt="WhatsApp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   )
